3c7dbedfb7ea6760fc3e74c696b27f3dc6044fd3
Dantallope <dantebraccialini@gmail.com>
2023-01-09 22:36:32 -0700

    spoiled potatoes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 const kitKlick = document.getElementById("kitKlick");
+const spoiled = document.getElementById("spoiled");
 
 kitKlick.addEventListener("click",function(){
     window.location.href = "https://dantallope.github.io/TGG-Project1/"
+})
+spoiled.addEventListener("click",function(){
+    window.location.href = "https://github.com/Dantallope/SpoiledPotatoes"
 })
