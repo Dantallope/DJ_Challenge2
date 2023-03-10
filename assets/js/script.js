23b198b296c70458c12fc902f952901aae68cd57
Dantallope <dantebraccialini@gmail.com>
2023-03-09 22:35:13 -0700

    last ever commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 const kitKlick = document.getElementById("kitKlick");
 const spoiled = document.getElementById("spoiled");
+const bigboi = document.getElementById("bigboi");
 
 kitKlick.addEventListener("click",function(){
     window.location.href = "https://dantallope.github.io/TGG-Project1/"
 })
 spoiled.addEventListener("click",function(){
     window.location.href = "https://github.com/Dantallope/SpoiledPotatoes"
+})
+bigboi.addEventListener("click",function(){
+    window.location.href = "https://big-boi-budgeting.herokuapp.com"
 })
